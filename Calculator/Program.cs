9d8c88eba7454da5a9c3097f788a9e5aa394f302
Sara Stjernberg <sara_stjernberg@hotmail.com>
2021-09-01 09:15:06 +0200

    Methods for calculations

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,8 +12,15 @@ namespace Calculator
             while (keepCalculating)
             {
 
-                Console.Write("Enter a calculation method: +, -, / or * (or -1 to exit): ");
+                
+
+
+                Console.Write("Enter a calculation method: +, -, / or * (or e to exit): ");
                 char calculationChoice = char.Parse(Console.ReadLine());
+
+                
+
+
                 try
                 {
                     switch (calculationChoice)
@@ -30,14 +37,18 @@ namespace Calculator
                         case '*':
                             RunMultiplication();
                             break;
-                        case '#':
+                        case 'e':
                             keepCalculating = false;
                             break;
+                        default:
+                            Console.WriteLine("That is not a valid option");
+                            break;
+
                     }
                 }
                 catch
                 {
-
+                    Console.WriteLine("That is not a valid option");
                 }
 
             }
@@ -46,19 +57,55 @@ namespace Calculator
         private static void RunSubtraction()
         {
             Console.WriteLine("Subtraction!");
+           
+            Console.WriteLine("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("-");
+            Console.WriteLine("Enter the second number: ");
+            int num2 = int.Parse (Console.ReadLine());
+            int difference = num1 - num2;
+            Console.WriteLine($"{num1} - {num2} = {difference}");
+
         } 
         private static void RunAddition()
         {
             Console.WriteLine("Addition!");
+
+            Console.WriteLine("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("+");
+            Console.WriteLine("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {sum}");
         } 
-        private static void RunDivision()
+        private static void RunDivision() 
         {
             Console.WriteLine("Division!");
+
+            Console.WriteLine("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("/");
+            Console.WriteLine("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int quota = num1 / num2;
+            Console.WriteLine($"{num1} / {num2} = {quota}");
         } 
         
         private static void RunMultiplication()
         {
             Console.WriteLine("Multiplication!");
+
+            Console.WriteLine("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("*");
+            Console.WriteLine("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int product = num1 * num2;
+            Console.WriteLine($"{num1} * {num2} = {product}");
+
         }
+
+      
     }
 }
